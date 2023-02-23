24c014f484889cef44977857e20b0dcf7cc2359d
Arthur <arthur.daes@gmail.com>
2023-02-23 16:39:56 -0300

    update, need tests

--- vector.c
+++ vector.c
@@ -7,39 +7,74 @@
 
 #include "vector.h"
 
-vector* vector_create_with(size_t capacity, size_t element_size) {
-    vector *vec = malloc(sizeof(vector));
-    vec->data = malloc(capacity * element_size);
+vector *vector_create_with(size_t capacity, size_t element_size) {
+    vector *vector = malloc(sizeof(vector));
+    vector->data = malloc(capacity * element_size);
 
-    vec->element_size = element_size;
-    vec->capacity = capacity;
-    vec->size = 0;
+    vector->element_size = element_size;
+    vector->capacity = capacity;
+    vector->read_index = 0;
+    vector->size = 0;
 
-    return vec;
+    return vector;
 }
 
+vector *vector_create(size_t element_size) {
+    vector *vector = malloc(sizeof(vector));
+    vector->data = malloc(VECTOR_DEFAULT_CAPACITY * element_size);
 
-vector* vector_create(size_t element_size) {
-    vector *vec = malloc(sizeof(vector));
-    vec->data = malloc(DEFAULT_CAPACITY * element_size);
+    vector->element_size = element_size;
+    vector->capacity = VECTOR_DEFAULT_CAPACITY;
+    vector->read_index = 0;
+    vector->size = 0;
 
-    vec->element_size = element_size;
-    vec->capacity = DEFAULT_CAPACITY;
-    vec->size = 0;
-
-    return vec;
+    return vector;
 }
 
-void vector_destroy(vector* vec) {
-    free(vec->data); vec->data = NULL;
-    free(vec); vec = NULL;
-}
-
-void vector_push(vector *v, const void *value) {
+void vector_push(vector *v, const void *element) {
     if (v->size == v->capacity) {
         v->capacity *= 2;
         v->data = realloc(v->data, v->capacity * v->element_size);
     }
-    memcpy((char*)v->data + v->size * v->element_size, value, v->element_size);
+    memcpy((char*)v->data + v->size * v->element_size, element, v->element_size);
     v->size++;
+}
+
+void* vector_peek(vector *v) {
+    if (v->size == 0) return NULL;
+
+    size_t last_index = (v->size - 1) * v->element_size;
+    return (char*)v->data + last_index;
+}
+
+void vector_pop(vector *v) {
+    if (v->size == 0) return;
+
+    size_t last_index = (v->size - 1) * v->element_size;
+    v->size--;
+    memset((char*)v->data + last_index, 0, v->element_size);
+}
+
+void *vector_read(vector *v) {
+    if (v->size == 0 || v->read_index == v->size) return NULL;
+
+    void *element = (char*)v->data + v->read_index * v->element_size;
+    v->read_index++;
+
+    return element;
+}
+
+void *vector_read_at(vector *v, size_t index) {
+    if (v->size == 0 || index >= v->size) return NULL;
+
+    return (char*)v->data + index * v->element_size;
+}
+
+void vector_rewind(vector *v) {
+    v->read_index = 0;
+}
+
+void vector_destroy(vector *v) {
+    free(v->data); v->data = NULL;
+    free(v); v = NULL;
 }
--- vector.h
+++ vector.h
@@ -8,11 +8,20 @@ typedef struct vector {
     size_t size;         // Number of elements in the vector
     size_t capacity;     // Number of elements the vector can hold
     size_t element_size; // Size of each element in bytes
+    size_t read_index;   // index of the element to read (left to right)
 } vector;
 
-#define DEFAULT_CAPACITY 10
+#define VECTOR_DEFAULT_CAPACITY 42
 
-vector* vector_create_with(size_t capacity, size_t element_size);
-vector* vector_create(size_t element_size);
-void vector_destroy(vector* vec);
-void vector_push(vector *v, const void *value);
+vector *vector_create_with(size_t capacity, size_t element_size);
+vector *vector_create(size_t element_size);
+void vector_destroy(vector *v);
+
+void vector_push(vector *v, const void *element);
+
+void vector_pop(vector *v);
+void *vector_peek(vector *v);
+void *vector_read(vector *v);
+void *vector_read_at(vector *v, size_t index);
+
+void vector_rewind(vector *v);
